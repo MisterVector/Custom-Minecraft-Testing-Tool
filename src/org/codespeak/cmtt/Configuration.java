bc8db82d48ba14273c3148ecece4d925561c10c4
MisterVector <whizkid3000@hotmail.com>
2020-07-20 18:01:47 -0700

    Added Configuration class

---
+++ src/org/codespeak/cmtt/Configuration.java
@@ -0,0 +1,14 @@
+package org.codespeak.cmtt;
+
+/**
+ * A configuration class
+ *
+ * @author Vector
+ */
+public class Configuration {
+
+    public static final String PROGRAM_VERSION = "0.0.0";
+    public static final String PROGRAM_NAME = "Custom Minecraft Testing Tool";
+    public static final String PROGRAM_TITLE = PROGRAM_NAME + " v" + PROGRAM_VERSION;
+    
+}
