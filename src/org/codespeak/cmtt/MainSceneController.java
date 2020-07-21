e5ca247cbe7675c8b5296156ea6feff951fed4a9
MisterVector <whizkid3000@hotmail.com>
2020-07-20 17:12:01 -0700

    Initial commit

---
+++ src/org/codespeak/cmtt/MainSceneController.java
@@ -0,0 +1,31 @@
+package org.codespeak.cmtt;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * Controller for the main scene
+ *
+ * @author Vector
+ */
+public class MainSceneController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
