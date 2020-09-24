66919b5b33987c904ce423240d0065e06fe6ccf1
MisterVector <whizkid3000@hotmail.com>
2020-09-24 09:07:24 -0700

    Moved the servers folder into the profiles folder

--- src/org/codespeak/cmtt/Configuration.java
+++ src/org/codespeak/cmtt/Configuration.java
@@ -12,9 +12,9 @@ public class Configuration {
     public static final String PROGRAM_VERSION = "0.0.0";
     public static final String PROGRAM_NAME = "Custom Minecraft Testing Tool";
     public static final String PROGRAM_TITLE = PROGRAM_NAME + " v" + PROGRAM_VERSION;
-    public static final String SERVERS_FOLDER = "servers";
     public static final String PROFILES_FOLDER = "profiles";
     public static final String DEVELOPMENT_FOLDER = PROFILES_FOLDER + File.separator + "development";
+    public static final String SERVERS_FOLDER = PROFILES_FOLDER + File.separator + "servers";
     public static final String DATA_FILE = "data.json";
     
 }
